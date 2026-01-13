1c1a0acadec3b26828e9c649991091ab258d223f
Mario Reder <mario.reder@pm.me>
2026-01-13 21:51:06 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -31,7 +31,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
-  "VITE_SEO_THEME_COLOR": "#fb7923",
+  "VITE_SEO_THEME_COLOR": "#e87149",
   "VITE_SEO_KEYWORDS": "Claude",
   "VITE_AVAILABLE_LANGUAGES": "en,zh,tc,ja,es,ko,vi,de,fr,ru,id,tr,it,pt,uk,pl,nl",
   "VITE_USE_CUSTOM_PNL_POSTERS": "true",
